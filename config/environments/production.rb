efe4280ba1f592a6b81fce7d344096cde56d64a4
wanbei714-jpg <wanbei714@gmail.com>
2026-05-04 22:19:08 +0800

    Set active storage variant processor to mini_magick

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,3 +129,4 @@ end
 %w[render_template render_partial render_collection].each do |event|
   ActiveSupport::Notifications.unsubscribe "#{event}.action_view"
 end
+config.active_storage.variant_processor = :mini_magick
